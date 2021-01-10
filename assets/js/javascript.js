c8d9ea74e57d7d0976486ed3b4da6bbfa13d2e22
echo <miyunniecho@gmail.com>
2021-01-09 23:37:52 -0800

    quiz questions array

---
+++ assets/js/javascript.js
@@ -0,0 +1,56 @@
+// Assignment Code
+
+
+let questions = [
+    {question : "Which is the correct HTML element to link the Javascript file?",
+        choiceA : "<p>",
+        choiceB : "<img src",
+        choiceC : "<script>",
+            correctAnswer : "C"},
+
+    {question : "Which syntax would create a for loop?", 
+        choiceA : "(i=0, i > .length; i++",
+        choiceB : "(i=0; i < .length; i++",
+        choiceC : "(i=0; i = length; i++",
+            correctAnswer : "B"},
+
+    {question : "How would you call a function named 'myCats"
+        choiceA : "let function = myCats",
+        choiceB : "call myCats()",
+        choiceC : "myCats()",
+            correctAnswer : "C"}
+    
+    {question : "What is the correct way to write an array?",
+    choiceA : "let desserts = ["ice cream", "pie", "cake", "cookies"]",
+    choiceB : "let desserts = {ice cream, pie, cake, cookies}",
+    choiceC : "desserts = ice cream, pie, cake, cookies",
+        correctAnswer : "A"},
+    
+    {question : "How do you round a number to the nearest integer?"
+        choiceA : "Math.round()",
+        choiceB : "round()",
+        choiceC : "mathround()",
+            correctAnswer : "A"},
+
+    {question : "What is true about booleans?"
+        choiceA : "they can only have one of two values"
+        choiceB : "it can be a true or false statement",
+        choiceC : "both of the above is correct",
+            correctAnswer : "C"},
+    
+    {question : "What does DOM stand for?"
+        choiceA : "decorated objects model",
+        choiceB : "document object model",
+        choiceC : "directory oriented mode"
+            correctAnswer : "B"}
+]
+
+// When start button is clicked, timer and quiz starts
+
+// Questions should appear after the previous one is answered
+
+// Incorrect answers subtract time from the clock
+
+// Game over when all questions or answered or timer reaches 0 
+
+// Use localstorage to save initials and scores
