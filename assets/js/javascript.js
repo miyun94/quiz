35cbfe27b0013fc5c2c4ec020e2da2496a107bb5
echo <miyunniecho@gmail.com>
2021-01-16 23:49:00 -0800

    timer quiz

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,56 +1,109 @@
-// Assignment Code
+let currQuestion = 0;
+let question = [
+    {
+        question: "Which is the correct HTML element to link the Javascript file?",
+        choiceA: "<p>",
+        choiceB: "<img src",
+        choiceC: "<script>",
+        correctAnswer: "C"
+    },
 
+    {
+        question: "Which syntax would create a for loop?",
+        choiceA: "(i=0, i > .length; i++)",
+        choiceB: "(i=0; i < .length; i++)",
+        choiceC: "(i=0; i = length; i++)",
+        correctAnswer: "B"
+    },
 
-let questions = [
-    {question : "Which is the correct HTML element to link the Javascript file?",
-        choiceA : "<p>",
-        choiceB : "<img src",
-        choiceC : "<script>",
-            correctAnswer : "C"},
+    {
+        question: "How would you call a function named 'myCats",
+        choiceA: "let function = myCats",
+        choiceB: "call myCats()",
+        choiceC: "myCats()",
+        correctAnswer: "C"
+    },
 
-    {question : "Which syntax would create a for loop?", 
-        choiceA : "(i=0, i > .length; i++",
-        choiceB : "(i=0; i < .length; i++",
-        choiceC : "(i=0; i = length; i++",
-            correctAnswer : "B"},
+    {
+        question: "How do you round a number to the nearest integer?",
+        choiceA: "Math.round()",
+        choiceB: "round()",
+        choiceC: "mathround()",
+        correctAnswer: "A"
+    },
 
-    {question : "How would you call a function named 'myCats"
-        choiceA : "let function = myCats",
-        choiceB : "call myCats()",
-        choiceC : "myCats()",
-            correctAnswer : "C"}
-    
-    {question : "What is the correct way to write an array?",
-    choiceA : "let desserts = ["ice cream", "pie", "cake", "cookies"]",
-    choiceB : "let desserts = {ice cream, pie, cake, cookies}",
-    choiceC : "desserts = ice cream, pie, cake, cookies",
-        correctAnswer : "A"},
-    
-    {question : "How do you round a number to the nearest integer?"
-        choiceA : "Math.round()",
-        choiceB : "round()",
-        choiceC : "mathround()",
-            correctAnswer : "A"},
+    {
+        question: "What is true about booleans?",
+        choiceA: "they can only have one of two values",
+        choiceB: "it can be a true or false statement",
+        choiceC: "both of the above is correct",
+        correctAnswer: "C"
+    },
 
-    {question : "What is true about booleans?"
-        choiceA : "they can only have one of two values"
-        choiceB : "it can be a true or false statement",
-        choiceC : "both of the above is correct",
-            correctAnswer : "C"},
-    
-    {question : "What does DOM stand for?"
-        choiceA : "decorated objects model",
-        choiceB : "document object model",
-        choiceC : "directory oriented mode"
-            correctAnswer : "B"}
+    {
+        question: "What does DOM stand for?",
+        choiceA: "decorated objects model",
+        choiceB: "document object model",
+        choiceC: "directory oriented mode",
+        correctAnswer: "B"
+    }
 ]
+let timeLeft = 0;
+
+// Create variable to keep score
+let score = 0
 
 // When start button is clicked, timer and quiz starts
+function timer() {
+    let timer = document.getElementById('timer')
+    timeLeft = 200;
+    let ticker = setInterval(function () {
+        timer.innerHTML = timeLeft;
+        if (timeLeft > 0) {
+            timeLeft--;
+        } else {
+            clearInterval(ticker);
+            $("#interface").html(`<h2>Time Up!</h2>`);
+        }
 
-// Questions should appear after the previous one is answered
+        if (currQuestion == question.length) {
+            clearInterval(ticker);
+        }
 
-// Incorrect answers subtract time from the clock
+    }, 1000)
+}
 
-// Game over when all questions or answered or timer reaches 0 
+//Questions should appear after the previous one is answered
+function changeQuestion() {
+    $("#interface").html(`<h2>${question[currQuestion].question}</h2>
+    <button onclick="checkAnswer('A')">${question[currQuestion].choiceA}</button>
+    <button onclick="checkAnswer('B')">${question[currQuestion].choiceB}</button>
+    <button onclick="checkAnswer('C')">${question[currQuestion].choiceC}</button>`);
+}
 
-// Use localstorage to save initials and scores
+//If answer is correct, move on; if answer is wrong then subtract time
+function checkAnswer(answer) {
+    if (answer == question[currQuestion].correctAnswer) {
+        // It's correct
+    } else {
+        if (timeLeft > 9) {
+            timeLeft -= 10;
+        } else {
+            timeLeft == 0;
+        }
+    }
+
+    if (currQuestion < question.length - 1) {
+        currQuestion++;
+        changeQuestion();
+    } else {
+        saveScore();
+        $("#interface").html(`<h2>End of Quiz</h2>
+            <p>Your Score: ${timeLeft}</p>
+            <p>Best Score: ${localStorage.getItem('currBest')}`);
+    }
+}
+    // Game over when all questions or answered or timer reaches 0
+
+    // Use localstorage to save initials and scores
+
